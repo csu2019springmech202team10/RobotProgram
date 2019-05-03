6d0bcc0f6941a077f8af7fd8945f477b589a9d71
ConnorWorrell <chemicalcoal1@gmail.com>
2019-05-03 16:46:32 -0600

    Branched from main, testing turing off motor when not in use and slowing motor down for the first second when turned on

--- Project/Main/Main/Main.ino
+++ Project/Main/Main/Main.ino
@@ -17,7 +17,7 @@
 #define M1 11
 DRV8834 stepper(MOTOR_STEPS, DIR, STEP, SLEEP, M0, M1);
 
-unsigned long TimeToStop = 0, NextStateTime = 0;
+unsigned long TimeToStop = 0, NextStateTime = 0, TimeStartState2 = 0, TimeInState2 = 0;
 
 void setup() {
     stepper.begin(200, MICROSTEPS);
@@ -34,19 +34,23 @@ void setup() {
     NextStateTime = millis();
 }
 
-int State = 2;
+int State = 10;
 //State 0 waiting for button press, State 1 when button press
 //State 1 wait 1 second before turning on the motors
 //State 2 Running motors, State 0 when button held for 1 second
+//State 10 Initilization state if moving directly to state 2
 
-int ButtonDownBefore = 0, LastState = 0;
+int ButtonDownBefore = 0, LastState = State;
 void loop() {
     
     stepper.disable();
     if(digitalRead(BUTTON) == 1 && State == 0 && ButtonDownBefore == 0) State = 1;
 
     if(State == 1 && LastState == 0) NextStateTime = millis() + 1000;
-    if(State == 1 && NextStateTime < millis()) State = 2;
+    if(State == 1 && NextStateTime < millis() || State == 10) {
+      TimeStartState2 = millis();
+      State = 2;
+    }
     
     
     if(State == 2 && digitalRead(BUTTON) == 1 && ButtonDownBefore == 0) TimeToStop = millis() + 1000;
@@ -57,11 +61,16 @@ void loop() {
     digitalWrite(LED,State == 0 || State == 1 ? 0 : 1);
     
     if(State == 2){
-      if(LastState == 1) stepper.enable();
+      TimeInState2 = millis() - TimeStartState2;
+
+      if(TimeInState2 < 1000) stepper.setRPM(100);
+      else stepper.setRPM(200);
+      
+      if(LastState == 1) stepper.setEnableActiveState(1);
       stepper.rotate(10);
     }
 
-    if(State == 0 && LastState == 2) stepper.disable();
+    if(State == 0 && LastState == 2) stepper.setEnableActiveState(0);
     
     LastState = State;
 }
